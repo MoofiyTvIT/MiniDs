213b70d2519e3fdafb3f669b669ac3f9200c6366
Muwaffaq Imam <muwaffaqimam@gmail.com>
2021-03-17 22:33:04 +0200

    Initial commit

---
+++ src/com/moofiyTv/minids/Main.java
@@ -0,0 +1,10 @@
+package com.moofiyTv.minids;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Welcome to Master Branch");
+        System.out.println("Every Branch is Related to a group of lessons");
+    }
+}
